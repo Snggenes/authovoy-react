577a8b6a74e298b932ad375003217c1bb6261b01
Enes <snggenes@gmail.com>
2025-01-09 18:20:42 +0100

    trial-1

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const serverUrl = import.meta.env.VITE_SERVER_URL;
+      if (!serverUrl) {
+        console.error("Server URL not found");
+        return;
+      }
+      const response = await fetch(`${import.meta.env.VITE_SERVER_URL}`);
+      const data = await response.json();
+      setMessage(data.message);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>My App</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
