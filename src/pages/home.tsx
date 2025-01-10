e2dcba845658cf65f5da7f95126bc83077c06743
Enes <snggenes@gmail.com>
2025-01-10 23:07:41 +0100

    trial-5

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const response = await fetch(`${import.meta.env.VITE_SERVER_URL}`);
+      if (!response.ok) {
+        setMessage("Error fetching message");
+        return;
+      }
+      const data = await response.json();
+      setMessage(data.message);
+    };
+    fetchMessage();
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
+      <p>{message}</p>
     </div>
   );
 }
